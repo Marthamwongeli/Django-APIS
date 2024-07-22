37c6fcbe3cc9f4d524b21cc2ee4fef6459a2f1f4
Marthamwongeli <marthamutuaa@gmail.com>
2024-07-22 16:20:16 +0300

    added files

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,21 @@
-from django.urls import path, include
+from django.urls import path
 from .views import StudentListView
+from .views import TeacherListView
+from .views import CourseListView
+from .views import ClassPeriodListView
 from .views import StudentDetailView
+from .views import CoursesDetailView
+from .views import ClassPeriodDetailView
+from .views import TeacherDetailView
+
 
 urlpatterns = [
     path('student/', StudentListView.as_view(), name = 'student_list_view'),
-    path('student/<int:id>/',StudentDetailView.as_view(),name= 'StudentDetailView')
+    path("teachers/", TeacherListView.as_view(),name="teacher_list_view"),
+    path("courses/", CourseListView.as_view(),name="course_list_view"),
+    path("classPeriod/",ClassPeriodListView.as_view(),name="class_Period_list_view"),
+    path("students/<int:id>/",StudentDetailView.as_view(),name="student_detailview"),
+    path("courses/<int:id>/",CoursesDetailView.as_view(),name="course_list_view" ),
+    path("classPeriod/<int:id>/",ClassPeriodDetailView.as_view(),name="class_Period_detailview"),
+    path("teachers/<int:id>/",TeacherDetailView.as_view(),name="teacher_detailview")
 ]
